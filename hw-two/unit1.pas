264003ec72f78233cd1dd26175679d15e5bc2698
sla6ak <slabakxaker@gmail.com>
2022-11-06 17:52:02 +0200

    домашка два

---
+++ hw-two/unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Memo1: TMemo;
+    procedure BitBtn1Click(Sender: TObject);
+    procedure BitBtn2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+begin
+  label2.caption:=edit1.text;
+  memo1.Text:=edit1.Text;
+end;
+
+procedure TForm1.BitBtn2Click(Sender: TObject);
+begin
+  close
+end;
+
+end.
+
